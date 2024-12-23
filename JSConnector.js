e323b18faf045bcd2487a7a7fd552557ff9ad1a6
Luqmus <135509819+LuQmu5@users.noreply.github.com>
2024-12-23 21:57:29 +0400

    Update JSConnector.js

--- JSConnector.js
+++ JSConnector.js
@@ -1,5 +1,27 @@
-function checkInstance() {
-  alert(myGameInstance != null);
+const VK_PUBLIC_ID = 1;
+const STORY_IMAGE_URL = "url";
+const UNITY_CONNECTOR_NAME = "UnityConnector"
+
+
+function checkSubscribe()
+{
+  alert("check sub");
 }
 
-setTimeout(checkInstance, 10000);
+function firstPromoUse()
+{
+  alert("first promo use");
+}
+
+function secondPromoUse()
+{
+  alert("second promo use);
+}
+
+jQuery(document).ready(function($)
+{
+  vkBridge.send('VKWebAppInit');
+
+  subToAppShareEvent();
+  subToPostStoryEvent();
+}); 
